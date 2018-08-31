808e6faee2fba058eb55862e188e0ac2145a3ebf
刘帅才 <liushuaicai@liushuaicaideMacBook-Pro.local>
2018-08-31 14:21:05 +0800

    Site updated: 2018-08-31 14:21:05

--- js/syntax.js
+++ js/syntax.js
@@ -1,12 +1,14 @@
-$('.gutter>pre>div').each(function(){
+$('.gutter>pre>span').each(function(){
 	var index = $(this).html();
 	if(index > 15){
 		$(this).css('display','none');
-		$(this).parents('.gutter').next('.code').find('div').eq(index).css('display','none');
+		$(this).next('br').css('display','none');
+		$(this).parents('.gutter').next('.code').find('.line').eq(index).css('display','none');
+		$(this).parents('.gutter').next('.code').find('br').eq(index).css('display','none');
 	}
 })
-$('.gutter>pre>div:last-child').each(function(){
-	var last = $(this).html();
+$('.gutter>pre').each(function(){
+	var last = $(this).find('.line').last().html();
 	if(last > 15){
 		var tr = "<tr style='color:#ddd'><td colspan='2'><a onclick='showall(this)' data='0' href='javascript:;'>>> 查看全部代码</a></td></tr>";
 		$(this).parents('tbody').append(tr);
@@ -16,15 +18,18 @@ $('.gutter>pre>div:last-child').each(function(){
 function showall(obj){
 	var data = $(obj).attr('data');
 	if(data == '0'){
-		$(obj).parents('tbody').find('div').css('display','block');
+		$(obj).parents('tbody').find('.line').css('display','block');
+		$(obj).parents('tbody').find('br').css('display','block');
 		$(obj).html('>> 隐藏代码').attr('data','1');
 	}else{
 		$(obj).html('>> 查看全部代码').attr('data','0');
-		$(obj).parents('tr').prev('tr').find('div').each(function(){
+		$(obj).parents('tr').prev('tr').find('.line').each(function(){
 			var index = $(this).html();
 			if(index > 15){
 				$(this).css('display','none');
-				$(this).parents('.gutter').next('.code').find('div').eq(index).css('display','none');
+				$(this).next('br').css('display','none');
+				$(this).parents('.gutter').next('.code').find('.line').eq(index).css('display','none');
+				$(this).parents('.gutter').next('.code').find('br').eq(index).css('display','none');
 			}
 		})
 	}
